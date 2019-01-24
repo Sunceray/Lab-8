22e72e785dad6f000fb1cffaaeaff376f4de0a6e
sunceray <sunceraypatterson@gmail.com>
2019-01-24 08:46:41 -0500

    init commit

---
+++ src/Lab8.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Lab8 {
+	
+	public static void main(String[] args) {
+		
+	   Scanner userInput = new Scanner(System.in);
+	   
+	   String[] names = new String[7];
+       
+	   names[0] = "Mike";
+	   names[1] = "Luke";
+	   names[2] = "Derek";
+	   names[3] = "Norvell";
+	   names[4] = "Lauren";
+	   names[5] = "Cyn";
+	   names[6] = "Kate";
+	   names[7] = "Mariah";
+	   
+	 
+
+}
+	
+	String[] stuFavFood = new String[7];
+	
+	stuFavFood[0] = "Pasta";
+	stuFavFood[1] = "Pizza";
+	stuFavFood[2] = "Sandwiches";
+	stuFavFood[3] = "Steak";
+	stuFavFood[4] = "Hot Wings";
+	stuFavFood[5] = "Hoagies";
+	stuFavFood[6] = "Cookies";
+	stuFavFood[7] = "Soup";
+	
+}
+
+	String[] stuHometown = new String[7];
+	
+		
+	stuHometown[0] = "Paris";
+	stuHometown[1] = "Eugene";
+    stuHometown[2] ="Portland";
+    stuHometown[3] = "Detroit";
+    stuHometown[5] = "LosAngeles";
+    stuHometown[6] = "Atlanta";
+    stuHometown[7] = "SanDeigo";
+    
+    }
+    
+  }
+			
+
